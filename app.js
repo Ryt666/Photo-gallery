63e69f76b97a4f233c8d83ac5a49d6f5c2aa04e3
Your Apollinaria <polinachka0603@gmail.com>
2023-02-14 21:05:12 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+ function slidesPlagin(activeSlides){
+    const slides= document.querySelectorAll('.slide')
+    slides[activeSlides].classList.add('active')
+
+    for (const slide of slides){
+        slide.addEventListener('click', () =>  {
+         clearActiveClasses()
+    
+            slide.classList.add('active')
+    
+        })
+    }
+    
+    function clearActiveClasses() {
+        slides.forEach((slide) => {
+            slide.classList.remove('active')
+        })
+    }
+ }
+
+ slidesPlagin(0)
